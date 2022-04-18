c8be6f84acdaac98fd89b7ba7d90242159c02b63
unknown <dsouza.jovin65@gmail.com>
2022-04-18 20:54:56 +0530

    react-17

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Homepage from './Pages/Homepage';
-import Coinpage from './Pages/Coinpage';
+// import Homepage from './Pages/Homepage';
+// import Coinpage from './Pages/Coinpage';
 import { makeStyles } from '@material-ui/core';
+import { lazy, Suspense } from 'react';
+
+const Homepage = lazy(() => import('./Pages/Homepage'));
+const Coinpage = lazy(() => import('./Pages/Coinpage'));
 
 function App() {
   const useStyles = makeStyles(() => ({
@@ -19,10 +23,12 @@ function App() {
     <BrowserRouter>
       <div className={classes.App}>
         <Header />
-        <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/coins/:id' element={<Coinpage />} />
-        </Routes>
+        <Suspense fallback={<div>Loading..</div>}>
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/coins/:id' element={<Coinpage />} />
+          </Routes>
+        </Suspense>
       </div>
     </BrowserRouter>
   );
