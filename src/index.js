f97a77fe4714bd2604b425d88be0378f74b721fc
unknown <dsouza.jovin65@gmail.com>
2023-02-03 21:57:41 +0530

    Crypto Dev

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import CryptoContext from './CryptoContext';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <CryptoContext>
-    <App />
-  </CryptoContext>
-);
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <CryptoContext>
-//       <App />
-//     </CryptoContext>
-//   </React.StrictMode>,
-//   document.getElementById('root')
+// root.render(
+//   <CryptoContext>
+//     <App />
+//   </CryptoContext>
 // );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CryptoContext>
+      <App />
+    </CryptoContext>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
